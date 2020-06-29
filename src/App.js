8b5ee7cc383281371de6b169ead1ef334e08a2ec
carolineglass <carolineglass89@gmail.com>
2020-06-29 11:00:23 -0400

    3 deliverables
    exit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ class App extends Component {
   }
 
   addNewWizard = (newWizard) => {
-    console.log(newWizard)
+    this.setState({
+      wizards: [...this.state.wizards, newWizard]
+    })
   }
 
   render() {
